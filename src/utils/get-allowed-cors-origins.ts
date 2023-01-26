e8357ff2824730220ee3d7b777e45760301f9f0c
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2023-01-26 21:53:09 +0100

    feat(cors): :sparkles: use env variable for cors

--- src/utils/get-allowed-cors-origins.ts
+++ src/utils/get-allowed-cors-origins.ts
@@ -2,4 +2,6 @@ export const getAllowedCorsOrigins = () => {
   if (process.env.NODE_ENV === 'dev') {
     return ['http://localhost:3001'];
   }
+
+  return [process.env.FRONTEND_BASE_URL as string];
 };
